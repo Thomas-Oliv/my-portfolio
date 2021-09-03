e1dd60139c771d6d6c2c2ed9b98128dc5746d963
Thomas-Oliv <drivemenuts766@gmail.com>
2021-09-02 18:01:04 -0700

    Added SEO to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout/layout"
+import Seo from "../components/layout/seo"
 
 const HomeIndex = ({ data}) => {
   const post = data.markdownRemark
 
   return (
     <Layout >
+      <Seo title="Home" />
       <article
         className="static-page"
         itemScope
